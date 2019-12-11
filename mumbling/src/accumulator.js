7f36c0ea77cfd16b90f2b000b9e92c756fc4e68f
zraso <zaira.rasool@hotmail.co.uk>
2019-12-11 16:29:23 +0000

    Refactor three letters

--- mumbling/src/accumulator.js
+++ mumbling/src/accumulator.js
@@ -7,7 +7,11 @@ function accumulator(letters) {
     second_letter = (array[1].repeat(2)).charAt(0).toUpperCase() + (array[1].repeat(2)).slice(1)
     return first_letter + "-" + second_letter
   } else if (letters === "abc") {
-    return "A-Bb-Ccc"
+    array = letters.split("")
+    first_letter = array[0].toUpperCase()
+    second_letter = (array[1].repeat(2)).charAt(0).toUpperCase() + (array[1].repeat(2)).slice(1)
+    third_letter = (array[2].repeat(3)).charAt(0).toUpperCase() + (array[2].repeat(3)).slice(1)
+    return first_letter + "-" + second_letter + "-" + third_letter
   } else {
     return "A-Bb-Ccc-Dddd"
   }
