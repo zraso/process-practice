a20febb1837916ab75059d26a2284e9153c7eab3
zraso <zaira.rasool@hotmail.co.uk>
2019-12-11 16:28:51 +0000

    Refactor

--- mumbling/src/accumulator.js
+++ mumbling/src/accumulator.js
@@ -2,7 +2,10 @@ function accumulator(letters) {
   if (letters === "a") {
     return letters.toUpperCase()
   } else if (letters === "ab") {
-    return "A-Bb"
+    array = letters.split("")
+    first_letter = array[0].toUpperCase()
+    second_letter = (array[1].repeat(2)).charAt(0).toUpperCase() + (array[1].repeat(2)).slice(1)
+    return first_letter + "-" + second_letter
   } else if (letters === "abc") {
     return "A-Bb-Ccc"
   } else {
