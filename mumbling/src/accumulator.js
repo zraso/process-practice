cfc781c609fd64473f4fa5d7f32bcea6941bcfdf
zraso <zaira.rasool@hotmail.co.uk>
2019-12-11 16:30:36 +0000

    Refactor using array map function

--- mumbling/src/accumulator.js
+++ mumbling/src/accumulator.js
@@ -3,9 +3,8 @@ function accumulator(letters) {
     return letters.toUpperCase()
   } else if (letters === "ab") {
     array = letters.split("")
-    first_letter = array[0].toUpperCase()
-    second_letter = (array[1].repeat(2)).charAt(0).toUpperCase() + (array[1].repeat(2)).slice(1)
-    return first_letter + "-" + second_letter
+    const new_array = array.map((letter, index) => letter.toUpperCase() + letter.repeat(index))
+    return new_array.join("-")
   } else if (letters === "abc") {
     array = letters.split("")
     first_letter = array[0].toUpperCase()
