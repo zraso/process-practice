f4ed122b7d90ecc53b001fb7bc954e44b43a261a
zraso <zaira.rasool@hotmail.co.uk>
2019-12-11 16:31:09 +0000

    Refactor using array map function for all tests

--- mumbling/src/accumulator.js
+++ mumbling/src/accumulator.js
@@ -1,24 +1,7 @@
 function accumulator(letters) {
-  if (letters === "a") {
-    return letters.toUpperCase()
-  } else if (letters === "ab") {
-    array = letters.split("")
-    const new_array = array.map((letter, index) => letter.toUpperCase() + letter.repeat(index))
-    return new_array.join("-")
-  } else if (letters === "abc") {
-    array = letters.split("")
-    first_letter = array[0].toUpperCase()
-    second_letter = (array[1].repeat(2)).charAt(0).toUpperCase() + (array[1].repeat(2)).slice(1)
-    third_letter = (array[2].repeat(3)).charAt(0).toUpperCase() + (array[2].repeat(3)).slice(1)
-    return first_letter + "-" + second_letter + "-" + third_letter
-  } else {
-    array = letters.split("")
-    first_letter = array[0].toUpperCase()
-    second_letter = (array[1].repeat(2)).charAt(0).toUpperCase() + (array[1].repeat(2)).slice(1)
-    third_letter = (array[2].repeat(3)).charAt(0).toUpperCase() + (array[2].repeat(3)).slice(1)
-    fourth_letter = (array[3].repeat(4)).charAt(0).toUpperCase() + (array[3].repeat(4)).slice(1)
-    return first_letter + "-" + second_letter + "-" + third_letter + "-" + fourth_letter
-  }
+  array = letters.split("")
+  const new_array = array.map((letter, index) => letter.toUpperCase() + letter.repeat(index))
+  return new_array.join("-")
 }
 
 module.exports = accumulator;
